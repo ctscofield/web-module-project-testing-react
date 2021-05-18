7d082d4b27eb565886ab69ab96c60cd2ca16e626
ctscofield <ctscofield@gmail.com>
2021-05-18 10:50:52 -0600

    fini

--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -10,31 +10,36 @@ const testEpisode = {
     image: "http://static.tvmaze.com/uploads/images/medium_landscape/67/168918.jpg",
     season: 1,
     number: 1,
-    summary: "",
+    summary: "this is a test",
     runtime: 1
 }
 
 const testEpisodeWithoutImage = {
-    //Add in approprate test data structure here.
-}
+    id:1,
+    name: "",
+    image: null,
+    season: 1,
+    number: 1,
+    summary: "this is a test",
+    runtime: 1}
 
 test("renders without error", () => {
-    const fakeFunc = jest.fn();
-    render(<App getData={fakeFunc}/>);
+    render(<Episode episode={testEpisode}/>);
     
 });
 
 test("renders the summury test passed as prop", ()=>{
-    render(<App testEpisode={testEpisode}/>);
-    const butt = screen.getByRole("button");
-    userEvent.click(butt);
-    const summ = screen.queryByLabelText("summary");
-    expect(summ).not.toBeInTheDocument();
-    expect(summ).toBeFalsy();
+    render(<Episode episode={testEpisode}/>);
+    const summary = screen.queryByText(/this is a test/i);
+    expect(summary).toBeInTheDocument();
+    expect(summary).toBeTruthy();
+    expect(summary).toHaveTextContent("this is a test");
 });
 
 test("renders default image when image is not defined", ()=>{
-    
+    render(<Episode episode={testEpisodeWithoutImage}/>);
+    const image = screen.queryByAltText("./stranger_things.png");
+    expect(image).toBeInTheDocument();
 })
 
 //Tasks
